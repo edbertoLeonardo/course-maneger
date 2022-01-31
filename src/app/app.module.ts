4bdecd6ecd4e24e5ea049fa7b59210dacc0754a7
edbertoLeonardo <edbleonardo@gmail.com>
2022-01-31 18:02:50 -0300

    incluindo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,35 @@ import { AppComponent } from './app.component';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component copy';
+import { RouterModule } from '@angular/router';
+import { NotFound404Component } from './not-found-404/not-found404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StarComponent,
     CourseListComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    NotFound404Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent,
+      },
+      {
+        path: '**', component: NotFound404Component
+      }
+    ])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
